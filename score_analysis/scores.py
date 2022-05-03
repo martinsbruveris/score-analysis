Bugfix in CI calculation.

score_analysis/scores.py
@@ -431,6 +431,6 @@ class Scores:
         samples = self.bootstrap_metric(
             metric, nb_samples=nb_samples, method=method, ratio=ratio
         )  # (N, Y)
-        ci = np.quantile(samples, q=[alpha / 2.0, (1 - alpha) / 2.0], axis=0)  # (2, Y)
+        ci = np.quantile(samples, q=[alpha / 2.0, 1 - alpha / 2.0], axis=0)  # (2, Y)
         ci = np.moveaxis(ci, source=0, destination=-1)  # (Y, 2)
         return ci

score_analysis/tools.py
@@ -109,7 +109,7 @@ def roc_with_ci(
     else:
         # Take the worst ones
         lower = np.minimum(bootstrap_ci[:, 0], binomial_ci[:, 0])
-        upper = np.maximum(bootstrap_ci[:, 0], binomial_ci[:, 1])
+        upper = np.maximum(bootstrap_ci[:, 1], binomial_ci[:, 1])
 
     # Calculate the y-axis values
     if fnr is not None:
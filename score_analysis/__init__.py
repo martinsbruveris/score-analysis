8893530f738791baae2ec3af3c6e7f98755525de
Marian Schneider <marian.schnei@gmail.com>
2024-04-08 09:56:53 +0100

    Bump version: 0.2.2 → 0.2.3

--- score_analysis/__init__.py
+++ score_analysis/__init__.py
@@ -5,4 +5,4 @@ from .scores import BinaryLabel, BootstrapConfig, Scores, pointwise_cm
 from .showbias import BiasFrame, showbias
 from .tools import roc, roc_with_ci
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
